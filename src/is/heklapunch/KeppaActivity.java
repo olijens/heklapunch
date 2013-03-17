0205fa81054acaac4583e9f203626fe3138421d3
logip <logip@hi.is>
2013-03-17 13:49:46 +0000

    Logo komið í lag

--- src/is/heklapunch/KeppaActivity.java
+++ src/is/heklapunch/KeppaActivity.java
@@ -13,7 +13,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Date;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Iterator;
 import java.util.List;
 
