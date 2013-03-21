48ede1c85fa45c2f6f336c5ea39993b2d63461ae
robotis <jtm@robot.is>
2013-03-21 13:50:51 +0000

    Add license to BT lib

--- src/is/heklapunch/bluetooth/BlueToothInsecure.java
+++ src/is/heklapunch/bluetooth/BlueToothInsecure.java
@@ -1,7 +1,15 @@
 package is.heklapunch.bluetooth;
 /*
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * 
+ * 	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE.
  * */
 import java.io.IOException;
 import java.lang.reflect.Constructor;
@@ -21,8 +29,11 @@ import android.os.ParcelUuid;
 import android.util.Log;
 
 public class BlueToothInsecure {
+	@SuppressWarnings("serial")
 	static private class InUse extends RuntimeException {
 	}
+	
+	@SuppressWarnings("rawtypes")
 	public static BluetoothServerSocket listenUsingRfcommWithServiceRecord(BluetoothAdapter adapter, String name, UUID uuid, boolean encrypt) throws IOException {
     	try {
 			Class c_rfcomm_channel_picker = null;
@@ -37,7 +48,7 @@ public class BlueToothInsecure {
 			if(c_rfcomm_channel_picker == null)
 	    		throw new RuntimeException("can't find the rfcomm channel picker class");
     	
-	    	Constructor constructor = c_rfcomm_channel_picker.getDeclaredConstructor(UUID.class);
+			Constructor constructor = c_rfcomm_channel_picker.getDeclaredConstructor(UUID.class);
 	    	if(constructor == null)
 	    		throw new RuntimeException("can't find the constructor for rfcomm channel picker");
 	    	Object rfcomm_channel_picker = constructor.newInstance(new Object[] {uuid});
